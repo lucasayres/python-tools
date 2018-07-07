73d695106ee1703c0addaddb3205c142580921d0
Lucas Ayres <lucasayres@startonapp.com>
2018-07-07 10:36:58 -0300

    Ad tool for reverse a list or string.

---
+++ tools/reverse.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def reverse(input):
+    """Reverse the order of a list or string.
+
+    Args:
+        input (str/list): List or String.
+
+    Returns:
+        str/list: Return a reversed list or string.
+
+    """
+    return input[::-1]
