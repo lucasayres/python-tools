97a4f3f8fe69384133f4fea2f91bea6f774b2b97
Lucas Ayres <lucasayres@startonapp.com>
2018-07-09 01:25:08 -0300

    Open file in binary mode

--- tools/base64_to_string.py
+++ tools/base64_to_string.py
@@ -3,13 +3,13 @@ import base64
 
 
 def base64_to_string(input, output):
-    """Decode base64 file to plaintext.
+    """Decode base64 file to string.
 
     Args:
         input (str): Base64 file.
-        output (str): Plaintext file.
+        output (str): Text file.
 
     """
-    with open(input) as f:
+    with open(input, 'rb') as f:
         with open(output, 'w') as f1:
             f1.write(base64.b64decode(f.read()).decode('utf-8'))
