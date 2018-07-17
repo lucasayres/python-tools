e4dabab8a1692c649b15c3b07b5217dd46160c20
Lucas Ayres <lucasayres@startonapp.com>
2018-07-17 00:09:23 -0300

    Change yaml_to_json.py, write output json file in another file

--- tools/yaml_to_json.py
+++ tools/yaml_to_json.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
+import json
 import yaml
 
 
-def yaml_to_json(yaml_file):
+def yaml_to_json(yaml_file, json_file):
     """Convert YAML to JSON.
 
     Args:
-        yaml_file (str): YAML file name.
-
-    Returns:
-        json: Return JSON object.
+        yaml_file (str): Input YAML file name.
+        json_file (str): Output JSON file name.
 
     """
     with open(yaml_file) as f:
-        document = f.read()
-    return yaml.load(document)
+        with open(json_file, 'w') as f1:
+            f1.write(json.dumps(yaml.load(f.read()), ensure_ascii=False))
