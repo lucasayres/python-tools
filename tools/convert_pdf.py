0596c3b7b8fad24e9d114695fcb263f38b74ad50
Lucas Ayres <lucasayres@startonapp.com>
2018-07-19 19:12:18 -0300

    Add tool for convert PDF file to text or html

---
+++ tools/convert_pdf.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from io import BytesIO
+from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+from pdfminer.converter import TextConverter, HTMLConverter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+
+
+def convert_pdf(input_file, format='text', codec='utf-8'):
+    """Convert PDF file to text or html.
+
+    Args:
+        input_file (str): Input PDF file.
+        format (str): Format text.
+        codec (str): Codec for encode the text.
+
+    Returns:
+        str: Return text or html from PDF file.
+
+    """
+    manager = PDFResourceManager()
+    output = BytesIO()
+    laparams = LAParams()
+    if format == 'text':
+        converter = TextConverter(manager, output, codec=codec, laparams=laparams)
+    elif format == 'html':
+        converter = HTMLConverter(manager, output, codec=codec, laparams=laparams)
+    else:
+        raise ValueError('Provide format: text or html')
+
+    with open(input_file, 'rb') as f1:
+        interpreter = PDFPageInterpreter(manager, converter)
+        for page in PDFPage.get_pages(f1,
+                                      caching=True,
+                                      check_extractable=True):
+            interpreter.process_page(page)
+
+        converter.close()
+        text = output.getvalue()
+        output.close()
+
+    return text.decode()
