015b86641158b6fe6f4b0f818f565c7b9914c31d
Lucas Ayres <lucasayres@startonapp.com>
2018-07-22 21:07:56 -0300

    Add tool for convert binary to decimal.

---
+++ tools/binary_to_decimal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def binary_to_decimal(binary):
+    """Convert Binary to Decimal.
+
+    Args:
+        binary (str): Binary.
+
+    Returns:
+        int: Return decimal value.
+
+    """
+    return int(binary, 2)
