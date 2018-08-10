1782814169cb636d278b8485fe04509e5779c6a2
Lucas Ayres <lucasayres@startonapp.com>
2018-08-09 23:34:26 -0300

    Add tool for remove duplicates from a list

---
+++ tools/list_remove_duplicates.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+def list_remove_duplicates(dup_list):
+    """Remove duplicates from a list.
+
+    Args:
+        dup_list (list): List.
+
+    Returns:
+        list: Return a list of unique values.
+
+    """
+    return list(set(dup_list))
