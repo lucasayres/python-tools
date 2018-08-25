f1fc687258f253cb2fa9213e55bdc76d45bd6455
Lucas Ayres <lucasayres@startonapp.com>
2018-08-24 23:32:17 -0300

    Add tool for count number of folders in a diectory recursively

--- tools/count_directories.py
+++ tools/count_directories.py
@@ -2,18 +2,18 @@
 import os
 
 
-def count_files(path):
-    """Count number of files in a directory recursively.
+def count_directories(path):
+    """Count number of folders in a diectory recursively.
 
     Args:
         path (str): Directory.
 
     Returns:
-        int: Return number of files.
+        int: Return number of folders.
 
     """
     count = 0
     for root, dirs, files in os.walk(path):
-        for f in files:
+        for d in dirs:
             count += 1
     return count
